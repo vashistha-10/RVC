3aa35cced8db21d0c2a25e99e22fa45f1eb89b96
AMAN VASHISTHA <54052904+vashistha-10@users.noreply.github.com>
2021-06-25 21:03:28 +0530

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ContextProvider } from "./components/Context";
+import "./style.css";
+
+ReactDOM.render(
+	<ContextProvider>
+		<App />
+	</ContextProvider>,
+	document.getElementById("root")
+);
